8fbb16e5b8fdc093b83011fd93b3edf284467ba4
PierreOF <marcos.pierre@upe.br>
2024-11-30 19:59:46 -0300

    feat: criado adaptador de insercao de cliente

---
+++ src/main/java/com/pierre/hexagonal/adapters/out/InsertCustomerAdapter.java
@@ -0,0 +1,25 @@
+package com.pierre.hexagonal.adapters.out;
+
+import com.pierre.hexagonal.adapters.out.repository.CustomerRepository;
+import com.pierre.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.pierre.hexagonal.application.core.domain.Customer;
+import com.pierre.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InsertCustomerAdapter implements InsertCustomerOutputPort {
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void insert(Customer customer){
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+
+}
---
+++ src/main/java/com/pierre/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,13 @@
+package com.pierre.hexagonal.adapters.out.repository.mapper;
+
+import com.pierre.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import com.pierre.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface CustomerEntityMapper {
+
+    CustomerEntity toCustomerEntity(Customer customer);
+}
