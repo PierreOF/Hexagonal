091808e1e1e0b9df700cb70dd58d1f4df7c506d2
PierreOF <marcos.pierre@upe.br>
2024-11-30 21:09:28 -0300

    refactor: corrigido agora a interface é componente

--- src/main/java/com/pierre/hexagonal/application/ports/in/FindCustomerByIdInputPort.java
+++ src/main/java/com/pierre/hexagonal/application/ports/in/FindCustomerByIdInputPort.java
@@ -1,7 +1,10 @@
 package com.pierre.hexagonal.application.ports.in;
 
 import com.pierre.hexagonal.application.core.domain.Customer;
+import jdk.jfr.Category;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface FindCustomerByIdInputPort {
 
     Customer find(String id);
