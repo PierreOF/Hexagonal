c97f2b5516dfd20837973a0153c333a1e32480a5
PierreOF <marcos.pierre@upe.br>
2024-12-01 17:49:05 -0300

    Feat: adapter de delete customer

---
+++ src/main/java/com/pierre/hexagonal/adapters/out/DeleteCustomerByIdAdapter.java
@@ -0,0 +1,18 @@
+package com.pierre.hexagonal.adapters.out;
+
+import com.pierre.hexagonal.adapters.out.repository.CustomerRepository;
+import com.pierre.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteCustomerByIdAdapter implements DeleteCustomerByIdOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void delete(String id) {
+        customerRepository.deleteById(id);
+    }
+}
